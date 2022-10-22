9d4800ee0c62080ba592e79a6601a313bdafa8dc
Leona <leonardofcoceschin@gmail.com>
2022-10-22 16:49:05 -0300

    adicionado o lisTaskById

--- src/main/java/com/ceschin/todoapi/controller/TaskController.java
+++ src/main/java/com/ceschin/todoapi/controller/TaskController.java
@@ -3,7 +3,9 @@ package com.ceschin.todoapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,11 @@ public class TaskController {
     @GetMapping("/tasks")
     public List<Task> listarTasks(){
         return taskService.listAllTasks();
+    }
+
+    @GetMapping("/task/{id}")
+    public ResponseEntity<Task> listTaskById(@PathVariable(value = "id") Long id){
+        return taskService.getTaskById(id);
     }
 
 }
--- src/main/java/com/ceschin/todoapi/service/TaskService.java
+++ src/main/java/com/ceschin/todoapi/service/TaskService.java
@@ -1,8 +1,10 @@
 package com.ceschin.todoapi.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ceschin.todoapi.model.Task;
@@ -22,4 +24,11 @@ public class TaskService {
     public List<Task> listAllTasks(){
         return taskRepository.findAll();
     }
-}
+    
+    public ResponseEntity<Task> getTaskById(Long id){
+        return taskRepository.findById(id)
+           .map(task -> ResponseEntity.ok().body(task))
+           .orElse(ResponseEntity.notFound().build());
+    }
+
+}    
